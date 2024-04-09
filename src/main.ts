59a8ae4bfaf35bebdb5e274eea5d702ebf3d04c0
Rafael Morales <rafaelmoralesvenegas@gmail.com>
2024-04-09 12:26:27 -0400

    Arreglando problemas con data-source.ts

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import 'reflect-metadata';
 import { AppDataSource } from './infraestructure/db/sql/data-source';
 
 async function bootstrap() {
-  console.log('AAAA');
   AppDataSource.initialize()
     .then(async (ds) => {
-      const app = await NestFactory.create(AppModule);
-
       ds.query(`SELECT 1 + 2 AS "res"`).then((res) => console.log(res));
 
-      await app.listen(3000);
+      const app = await NestFactory.create(AppModule);
+
+      await app.listen(3000).then(() => console.log('Listening on port 3000'));
     })
     .catch((e: any) => console.log(e));
 }
